0a67ae646723b5c05b434b3b0d86f91637fc8893
timbaron <timothy.akiode@student.aul.edu.ng>
2022-08-01 22:41:37 +0100

    Initialized react-native-paper

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    /* A babel plugin that is only used in production. */
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
