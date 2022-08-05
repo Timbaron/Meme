9532bc4481f0d13cfee8029883dcb873e7254b67
timbaron <timothy.akiode@student.aul.edu.ng>
2022-08-05 16:17:33 +0100

    re-write image upload

--- components/meme/upload.js
+++ components/meme/upload.js
@@ -2,59 +2,70 @@ import React, { useState, useEffect } from 'react'
 import { Alert, Image, StyleSheet, View } from 'react-native'
 import { ActivityIndicator, Appbar, Button, RadioButton, Text, TextInput } from 'react-native-paper'
 import * as ImagePicker from 'expo-image-picker';
+// import { useFormik } from 'formik';
+// import RNFetchBlob from 'rn-fetch-blob'
 
 export default function Upload({ navigation, route }) {
     const [type, setType] = React.useState('clean');
     const token = route.params
-    const [uri, setUri] = useState(null);
+    const [url, setUrl] = useState(null);
     const [caption, setCaption] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [image, setImage] = useState(null);
-    const pickImage = async () => {
-        // No permissions request is necessary for launching the image library
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            // allowsEditing: true,
-            // aspect: [2, 4],
-            quality: 1,
-        });
 
-        console.log(result);
+    // const pickImage = async () => {
+    //     // No permissions request is necessary for launching the image library
+    //     let result = await ImagePicker.launchImageLibraryAsync({
+    //         mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    //         // allowsEditing: true,
+    //         // aspect: [2, 4],
+    //         quality: 1,
+    //     });
 
-        if (!result.cancelled) {
-            setUri(result.uri);
-            setImage(result);
-        }
-    };
+
+    //     if (!result.cancelled) {
+    //         console.log(result);
+    //         seturl(result.url);
+    //         setImage(result);
+    //     }
+    // };
 
     const resultHandler = (result) => {
-        if(result.message != ''){
-            setImage(null);
+        if (result.status == 'error') {
+            Alert.alert('Upload Failed','The URL entered is not an image URL')
             setIsLoading(false)
-            Alert.alert(result.message)
         }
+        else if (result.errors == []) {
+            Alert.alert('Upload Failed', 'Please enter a valid image URL')
+            setIsLoading(false)
+        }
+        else if (result.status == 'success') {
+            setIsLoading(false);
+            setUrl('')
+            setType('clean')
+            setCaption('')
+            navigation.pop(2);
+            Alert.alert('Upload Successful', 'Hope your meme is funny, I go just delete am in 2 seconds')
+        }
+        console.log(result);
     }
-    const UploadImage = () => {
+    const UploadImage = async () => {
         setIsLoading(true);
-        console.log(image);
         var myHeaders = new Headers();
+        myHeaders.append("Accept", "application/json");
         myHeaders.append("Authorization", `Bearer ${token}`);
 
         var formdata = new FormData();
-        formdata.append("caption", caption);
-        formdata.append("type", type);
-        formdata.append('meme', {type: 'image/jpg', uri: uri, name: 'uploadimagetmp.png'});
 
         var requestOptions = {
             method: 'POST',
             headers: myHeaders,
-            body: formdata, 
             redirect: 'follow'
         };
 
-        fetch("https://memejokes.herokuapp.com/api/meme/upload", requestOptions)
-            .then(response => response.text())
-            .then(result => resultHandler(JSON.parse(result)))
+        fetch(`https://memejokes.herokuapp.com/api/meme/upload?meme=${url}&type=${type}&caption=${caption}`, requestOptions)
+            .then(response => response.json())
+            .then(result => resultHandler(result))
             .catch(error => console.log('error', error));
     }
     const _goBack = () => {
@@ -70,21 +81,28 @@ export default function Upload({ navigation, route }) {
                 <Appbar.Content title="Upload Meme" />
             </Appbar.Header>
             <View style={styles.container}>
-                <Button icon="file" mode="outlined" onPress={pickImage} style={styles.button}>
+                {/* <Button icon="file" mode="outlined" onPress={pickImage} style={styles.button}>
                     Select Meme
-                </Button>
-                {uri && <Image source={{ uri: uri }} style={{ width: 200, height: 200 }} />}
+                </Button> */}
+                <TextInput
+                    label='Meme URL'
+                    mode="outlined"
+                    value={url}
+                    onChangeText={url => setUrl(url)}
+                    style={styles.input}
+                />
+                {/* {url && <Image source={{ url: url }} style={{ width: 200, height: 200 }} />} */}
                 <Text style={styles.label}>Select Meme type</Text>
-                    <RadioButton.Group onValueChange={newtype => setType(newtype)} value={type}>
-                        <View>
-                            <Text style={styles.label2}>Clean</Text>
-                            <RadioButton value="clean" />
-                        </View>
-                        <Text style={styles.label2}>Dark</Text>
-                        <RadioButton value="dark" />
-                        <View>
-                        </View>
-                    </RadioButton.Group>
+                <RadioButton.Group onValueChange={newtype => setType(newtype)} value={type}>
+                    <View>
+                        <Text style={styles.label2}>Clean</Text>
+                        <RadioButton value="clean" />
+                    </View>
+                    <Text style={styles.label2}>Dark</Text>
+                    <RadioButton value="dark" />
+                    <View>
+                    </View>
+                </RadioButton.Group>
                 <TextInput
                     label='Meme Caption (Optional)'
                     mode="outlined"
@@ -112,7 +130,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'white',
         alignItems: 'center',
-        justifyContent: 'center',        
+        justifyContent: 'center',
     },
     input: {
         margin: 20,
